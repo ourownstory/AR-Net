251b75c91529f3762fef4a461cccb37d1e3aa2ad
ourownstory <ourownstory@users.noreply.github.com>
2020-11-06 21:48:47 -0800

    fix tessts paths

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 import pathlib
+import shutil
 import logging
 
 ## lazy imports ala fastai2 style (needed for nice print functionality)
@@ -17,8 +18,8 @@ log.parent.setLevel("WARNING")
 
 DIR = pathlib.Path(__file__).parent.parent.absolute()
 data_path = os.path.join(DIR, "ar_data")
-data_name = os.path.join(data_path, "ar_3_ma_0_noise_0.100_len_10000")
-results_path = os.path.join(data_path, "test_results")
+
+results_path = os.path.join(data_path, "results_test")
 
 EPOCHS = 3
 
@@ -36,13 +37,13 @@ class IntegrationTests(unittest.TestCase):
         if self.save:
             if not os.path.exists(results_path):
                 os.makedirs(results_path)
-        df, data_config = arnet.load_from_file(data_path, data_name, load_config=True, verbose=self.verbose)
-
         # Hyperparameters
         n_epoch = 2
         valid_p = 0.2
         n_forecasts = 1  # Note: if more than one, must have a list of ar_param for each forecast target.
         sparsity = 0.3  # guesstimate
+        data_name = "ar_3_ma_0_noise_0.100_len_10000"
+        df, data_config = arnet.load_from_file(data_path, data_name, load_config=True, verbose=self.verbose)
 
         # sparse AR: (for non-sparse, set sparsity to 1.0)
         ar_order = int(1 / sparsity * data_config["ar_order"])
@@ -81,7 +82,7 @@ class IntegrationTests(unittest.TestCase):
                 ar_val=len(ar_params[0]), weights=coeff[0], ar=ar_params[0], save=self.save, savedir=results_path
             )
             arnet.plot_prediction_sample(preds, y, num_obs=100, save=self.save, savedir=results_path)
-            arnet.plot_error_scatter(preds, y, save=save, savedir=results_path)
+            arnet.plot_error_scatter(preds, y, save=self.save, savedir=results_path)
 
         if self.save:
             # Optional:save and create inference learner
@@ -89,7 +90,7 @@ class IntegrationTests(unittest.TestCase):
             model_name = "ar{}_sparse_{:.3f}_ahead_{}_epoch_{}.pkl".format(ar_order, sparsity, n_forecasts, n_epoch)
             learn.export(fname=os.path.join(results_path, model_name))
             # can be loaded like this
-            learn = arnet.load_learner(fname=os.path.join(results_path, model_name), cpu=True)
+            learn = load_learner(fname=os.path.join(results_path, model_name), cpu=True)
         # can unfreeze the model and fine_tune
         learn.unfreeze()
         learn.fit_one_cycle(1, lr_at_min / 100)
@@ -106,4 +107,8 @@ class IntegrationTests(unittest.TestCase):
         )
 
         if self.save:
-            os.rmdir(results_path)
+            shutil.rmtree(results_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
